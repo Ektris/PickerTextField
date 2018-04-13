c27647ed2ba8d3f39e7260a46e9f156be70cc9b9
Robert W. Carrier <rwcarrie@us.ibm.com>
2018-04-13 16:11:52 -0500

    Created basic PickerTextField

---
+++ PickerTextField/PickerTextField.swift
@@ -0,0 +1,114 @@
+//
+//  PickerTextField.swift
+//  PickerTextField
+//
+//  Created by Robert Carrier on 4/12/18.
+//  Copyright © 2018 rwc. All rights reserved.
+//
+
+import UIKit
+
+open class PickerTextField: UITextField, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    // MARK: - Properties
+    
+    fileprivate let picker = UIPickerView()
+    
+    fileprivate var _data = [String]()
+    
+    open var data: [String] {
+        set (values) {
+            var newValues = [""]
+            newValues.append(contentsOf: values)
+            self._data = newValues
+        }
+        get {
+            return self._data
+        }
+    }
+    
+    open var selectedValue: String?
+    
+    // MARK: - Initializers
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        initPickerView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initPickerView()
+    }
+    
+    private func initPickerView() {
+        self.delegate = self
+        
+        self.picker.autoresizingMask = [.flexibleHeight]
+        self.picker.translatesAutoresizingMaskIntoConstraints = false
+        self.picker.dataSource = self
+        self.picker.delegate = self
+        self.picker.showsSelectionIndicator = true
+        self.picker.isUserInteractionEnabled = true
+    }
+    
+    // MARK: - Selection handlers
+    
+    fileprivate func setSelection(_ row: Int) {
+        self.text = self._data[row]
+        self.selectedValue = self._data[row]
+    }
+    
+    public func clearSelection() -> Bool {
+        self.picker.selectRow(0, inComponent: 0, animated: false)
+        pickerView(self.picker, didSelectRow: 0, inComponent: 0)
+        return true
+    }
+    
+    // MARK: - UITextFieldDelegate
+    
+    public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        self.inputView = self.picker
+        
+        let dismissBar = UIToolbar(frame: .zero)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissEditor))
+        dismissBar.setItems([doneButton], animated: false)
+        dismissBar.sizeToFit()
+        dismissBar.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        dismissBar.translatesAutoresizingMaskIntoConstraints = false
+        self.inputAccessoryView = dismissBar
+        
+        return true
+    }
+    
+    open func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
+    open func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return clearSelection()
+    }
+    
+    @objc fileprivate func dismissEditor() {
+        self.resignFirstResponder()
+    }
+    
+    // MARK: - UIPickerViewDataSource
+    
+    open func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    open func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self._data.count
+    }
+    
+    // MARK: - UIPickerViewDelegate
+    
+    open func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self._data[row]
+    }
+    
+    open func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        setSelection(row)
+    }
+}
